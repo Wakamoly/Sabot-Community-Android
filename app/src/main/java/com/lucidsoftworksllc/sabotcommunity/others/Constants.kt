c01056966090a3ce237243cf8299291ece862d2f
Wakamoly <lucidsoftworksllc@gmail.com>
2020-10-01 21:40:19 -0400

    git commit
    Signed-off-by: Wakamoly <lucidsoftworksllc@gmail.com>

--- app/src/main/java/com/lucidsoftworksllc/sabotcommunity/others/Constants.kt
+++ app/src/main/java/com/lucidsoftworksllc/sabotcommunity/others/Constants.kt
@@ -2,8 +2,8 @@ package com.lucidsoftworksllc.sabotcommunity.others
 
 object Constants {
     const val BASE_URL = "https://sabotcommunity.com/"
-    private const val APP_VERSION = "v0.5.1/"
-    const val APP_VERSION_FINAL = "v0.5.1"
+    private const val APP_VERSION = "v0.5.0/"
+    const val APP_VERSION_FINAL = "v0.5.0.1"
     const val APP_GRADLE_VERSION = 59
     const val ROOT_URL = BASE_URL + "app/" + APP_VERSION
     const val URL_REGISTER = ROOT_URL + "registerUser.php"
