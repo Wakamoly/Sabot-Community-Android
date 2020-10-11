3c67ef05f1398bfd69ba24abbb842402c12a8f6c
Brendan Abbott <45652967+wakamoly@users.noreply.github.com>
2020-10-11 14:44:55 -0400

    git commit
    Signed-off-by: Wakamoly <lucidsoftworksllc@gmail.com>

---
+++ app/src/main/java/com/lucidsoftworksllc/sabotcommunity/others/SabotApp.java
@@ -0,0 +1,29 @@
+package com.lucidsoftworksllc.sabotcommunity.others;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.acra.ACRA;
+import org.acra.BuildConfig;
+import org.acra.annotation.AcraCore;
+import org.acra.annotation.AcraHttpSender;
+import org.acra.data.StringFormat;
+import org.acra.sender.HttpSender;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+class HiltApp extends Application{}
+
+@AcraCore(buildConfigClass = BuildConfig.class,
+        reportFormat = StringFormat.JSON,additionalSharedPreferences={"SharedPrefManager"})
+@AcraHttpSender(uri = "http://www.sabotcommunity.com/acra.php",httpMethod = HttpSender.Method.POST,basicAuthLogin = "yeetskeet",basicAuthPassword = "sabot")
+class ACRAApp extends HiltApp {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        ACRA.init(this);
+    }
+}
+
+public class SabotApp extends ACRAApp{}
