36b2fb288b1b3212ab843568428446f5f044121c
Wakamoly <lucidsoftworksllc@gmail.com>
2020-10-11 14:48:39 -0400

    git commit
    Signed-off-by: Wakamoly <lucidsoftworksllc@gmail.com>

--- app/src/main/java/com/lucidsoftworksllc/sabotcommunity/others/SabotApplication.kt
+++ app/src/main/java/com/lucidsoftworksllc/sabotcommunity/others/SabotApplication.kt
@@ -10,6 +10,7 @@ import org.acra.annotation.AcraHttpSender
 import org.acra.data.StringFormat
 import org.acra.sender.HttpSender
 
+/*
 @HiltAndroidApp
 open class HiltApplication : Application()
 
@@ -22,4 +23,4 @@ open class ACRAApplication : HiltApplication() {
     }
 }
 
-class SabotApplication : ACRAApplication()
+class SabotApplication : ACRAApplication()*/
